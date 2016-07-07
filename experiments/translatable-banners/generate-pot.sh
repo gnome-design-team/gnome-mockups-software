941c8ed1276166c0d8427813d92dfdcc5f8108a4
Jakub Steiner <jimmac@gmail.com>
2016-07-07 17:29:54 +0200

    / ‘experiments/translatable-banners/generate-pot.sh’

--- experiments/translatable-banners/generate-pot.sh
+++ experiments/translatable-banners/generate-pot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for file in *; do 
-  if [[ -d $file && ! "C" ]]; then
+  if [[ -d $file && ne "C" ]]; then
     echo $file 
-#    echo "itstool -o $file C/*svg"
+    echo "itstool -o $file C/*svg"
   fi
 done
