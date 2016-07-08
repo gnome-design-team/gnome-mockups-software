cb073bb494adb3c11305c6e868546adff9065dc2
Jakub Steiner <jimmac@gmail.com>
2016-07-08 13:11:12 +0200

    / ‘experiments/translatable-banners/translate.sh’

--- experiments/translatable-banners/translate.sh
+++ experiments/translatable-banners/translate.sh
@@ -6,7 +6,7 @@ for f in *; do
     msgfmt -o $f/$f.mo $f/$f.po
     itstool -m $f/$f.mo -o $f/ C/*svg
     for s in $f/*svg; do
-      p =`basename $s svg`png
+      p=`basename $s svg`png
       echo inkscape -e $p $s
     done
   fi
