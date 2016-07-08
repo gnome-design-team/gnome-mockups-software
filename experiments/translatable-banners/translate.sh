7df3b59e05bde2446df276b619115092cdb4c506
Jakub Steiner <jimmac@gmail.com>
2016-07-08 12:49:17 +0200

    / ‘experiments/translatable-banners/translate.sh’

--- experiments/translatable-banners/translate.sh
+++ experiments/translatable-banners/translate.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-msgfmt $1/$1.pot
+for f in *; do
+  if [[ -d $f ]]; then
+    echo "$f is a directory"
+  fi
+done
+
+#msgfmt -o $f/$f.mo $f/$f.po
+#itstool -m $f/$f.mo -o $f/ C/*svg
