d469eb092ac4300ac8dd42793b834dcb8a247193
Jakub Steiner <jimmac@gmail.com>
2016-07-08 12:51:43 +0200

    / ‘experiments/translatable-banners/translate.sh’

--- experiments/translatable-banners/translate.sh
+++ experiments/translatable-banners/translate.sh
@@ -3,8 +3,8 @@
 for f in *; do
   if [[ -d $f && ! "$f" == "C" ]]; then
     echo "translating into $f"
-    echo msgfmt -o $f/$f.mo $f/$f.po
-    echo itstool -m $f/$f.mo -o $f/ C/*svg
+    msgfmt -o $f/$f.mo $f/$f.po
+    itstool -m $f/$f.mo -o $f/ C/*svg
   fi
 done
 
